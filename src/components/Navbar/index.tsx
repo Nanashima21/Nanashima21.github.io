63613e05f3229197ebd523c479e141de033a179e
Keita Nakajima <confi2100@gmail.com>
2023-12-19 03:47:35 +0900

    fix: format

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./Navbar.css"
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav className="nav">
-            <a href="https://nanashima21.github.io">Nanashima21</a>
-            <a href="https://nanashima21.github.io/blog/">Blog</a>
-        </nav>
-    );
-}
+  return (
+    <nav className="nav">
+      <a href="https://nanashima21.github.io">Nanashima21</a>
+      {/* <a href="https://nanashima21.github.io/blog/">Blog</a> */}
+    </nav>
+  );
+};
 export default Navbar;
