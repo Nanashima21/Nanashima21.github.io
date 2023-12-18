6565864ccbaa65026b394492db71b39faa1a36d1
Keita Nakajima <confi2100@gmail.com>
2023-12-19 03:01:41 +0900

    add: history (BSC)

--- src/data/histories.ts
+++ src/data/histories.ts
@@ -1,70 +1,77 @@
-import type { timeline } from "../types"
+import type { timeline } from "../types";
 
 export const historiesData = [
-    {
-        title: "AtCoder ヒューリスティックレートが青色（1600）に到達",
-        text: "トヨタ自動車プログラミングコンテスト2023#6（AtCoder Heuristic Contest 026）でパフォーマンス1916を出し、入青しました!",
-        timestamp: "2023年11月",
-        image: "atcoder.png"
-    },
-    {
-        title: "株式会社Speee エンジニア職 秋インターンシップ（2Days）",
-        text: "DX領域の仮想のサービス立ち上げを題材に、新規事業立ち上げのSprint0にチームで取り組みました。不確実性の高い中で意思決定することの難しさを実感しましたが、実務レベルでビジネス側の視点から設計を考えたことはとても新鮮で貴重な体験になりました。",
-        timestamp: "2023年10月",
-        image: "speee.jpeg"
-    },
-    {
-        title: "株式会社エクサウィザーズ ソフトウェアエンジニアサマーインターンシップ（長期）",
-        text: "AI Softwareチームに所属し、実際の社会課題を解決することを目的としたアプリ開発をチームで取り組みました。Androidアプリでのクライアントとサーバーサイドの実装を行いました。",
-        timestamp: "2023年8月 〜 2023年9月",
-        image: "exawizards.png"
-    },
-    {
-        title: "応用情報技術者に合格",
-        text: "情報処理推進機構(IPA)が主催するIT関連の資格試験に合格しました。",
-        timestamp: "2023年4月",
-        image: "ipa.png"
-    },
-    {
-        title: "Ｓｋｙ株式会社 学生向けジョブ型長期インターンシップ",
-        text: "社内DX関連のシステム開発の作業に取り組んでいます。勤務データとヒアリング情報から稼働率を抽出するWebサービスの機能充実に携わり、WebフロントエンドとBFFにおける実装と不具合修正を行っています。",
-        timestamp: "2023年4月 〜 現在",
-        image: "sky.jpeg"
-    },
-    {
-        title: "AtCoder アルゴリズムレートが水色（1200）に到達",
-        text: "大和証券プログラミングコンテスト2022 Autumn (AtCoder Beginner Contest 277)でパフォーマンス1618を出し、入水しました!",
-        timestamp: "2022年11月",
-        image: "atcoder.png"
-    },
-    {
-        title: "法政大学 理工学部 ラーニングサポーター",
-        text: "学部4年時に低学年に対しての進路相談や個別指導を行っていました。主にC言語, 線形代数, 微分積分等を教えていました。",
-        timestamp: "2022年4月 〜 2023年2月",
-        image: "hosei.jpeg"
-    },
-    {
-        title: "JDLA Deep Learning for GENERAL 2020 #2に合格",
-        text: "一般社団法人日本ディープラーニング協会が実施するAIに関する検定試験に合格しました。",
-        timestamp: "2020年7月",
-        image: "jdla.jpeg"
-    },
-    {
-        title: "基本情報技術者に合格",
-        text: "情報処理推進機構(IPA)が主催するIT関連の資格試験に合格しました。",
-        timestamp: "2019年10月",
-        image: "ipa.png"
-    },
-    {
-        title: "飲食店でのアルバイト",
-        text: "大学の近くのお寿司屋さんでアルバイトをしていました。初めてのバイトということもあり、大変でしたがとても良い経験でした。",
-        timestamp: "2019年9月 〜 2021年3月",
-        image: "sushi.png"
-    },
-    {
-        title: "実用技能英語検定準1級に合格",
-        text: "公益財団法人日本英語検定協会が実施する日本の英語の語学検定に合格しました。",
-        timestamp: "2016年10月",
-        image: "eiken.png"
-    },
+  {
+    title: "MIXI Bug Shooting Challenge #10（1Day）",
+    text: "ユーザーからの問い合わせに対して、ログやソースコードからシステムの不具合を見つけ出し修正まで行うワークショップに参加しました。サーバーサイドの不具合調査でBigQueryなど経験したことのない技術に触れることができました。",
+    timestamp: "2023年12月",
+    image: "bsc.png",
+  },
+  {
+    title: "AtCoder ヒューリスティックレートが青色（1600）に到達",
+    text: "トヨタ自動車プログラミングコンテスト2023#6（AtCoder Heuristic Contest 026）でパフォーマンス1916を出し、入青しました!",
+    timestamp: "2023年11月",
+    image: "atcoder.png",
+  },
+  {
+    title: "株式会社Speee エンジニア職 秋インターンシップ（2Days）",
+    text: "DX領域の仮想のサービス立ち上げを題材に、新規事業立ち上げのSprint0にチームで取り組みました。不確実性の高い中で意思決定することの難しさを実感しましたが、実務レベルでビジネス側の視点から設計を考えたことはとても新鮮で貴重な体験になりました。",
+    timestamp: "2023年10月",
+    image: "speee.jpeg",
+  },
+  {
+    title:
+      "株式会社エクサウィザーズ ソフトウェアエンジニアサマーインターンシップ（長期）",
+    text: "AI Softwareチームに所属し、実際の社会課題を解決することを目的としたアプリ開発をチームで取り組みました。Androidアプリでのクライアントとサーバーサイドの実装を行いました。",
+    timestamp: "2023年8月 〜 2023年9月",
+    image: "exawizards.png",
+  },
+  {
+    title: "応用情報技術者に合格",
+    text: "情報処理推進機構(IPA)が主催するIT関連の資格試験に合格しました。",
+    timestamp: "2023年4月",
+    image: "ipa.png",
+  },
+  {
+    title: "Ｓｋｙ株式会社 学生向けジョブ型長期インターンシップ",
+    text: "社内DX関連のシステム開発の作業に取り組んでいます。勤務データとヒアリング情報から稼働率を抽出するWebサービスの機能充実に携わり、WebフロントエンドとBFFにおける実装と不具合修正を行っています。",
+    timestamp: "2023年4月 〜 現在",
+    image: "sky.jpeg",
+  },
+  {
+    title: "AtCoder アルゴリズムレートが水色（1200）に到達",
+    text: "大和証券プログラミングコンテスト2022 Autumn (AtCoder Beginner Contest 277)でパフォーマンス1618を出し、入水しました!",
+    timestamp: "2022年11月",
+    image: "atcoder.png",
+  },
+  {
+    title: "法政大学 理工学部 ラーニングサポーター",
+    text: "学部4年時に低学年に対しての進路相談や個別指導を行っていました。主にC言語, 線形代数, 微分積分等を教えていました。",
+    timestamp: "2022年4月 〜 2023年2月",
+    image: "hosei.jpeg",
+  },
+  {
+    title: "JDLA Deep Learning for GENERAL 2020 #2に合格",
+    text: "一般社団法人日本ディープラーニング協会が実施するAIに関する検定試験に合格しました。",
+    timestamp: "2020年7月",
+    image: "jdla.jpeg",
+  },
+  {
+    title: "基本情報技術者に合格",
+    text: "情報処理推進機構(IPA)が主催するIT関連の資格試験に合格しました。",
+    timestamp: "2019年10月",
+    image: "ipa.png",
+  },
+  {
+    title: "飲食店でのアルバイト",
+    text: "大学の近くのお寿司屋さんでアルバイトをしていました。初めてのバイトということもあり、大変でしたがとても良い経験でした。",
+    timestamp: "2019年9月 〜 2021年3月",
+    image: "sushi.png",
+  },
+  {
+    title: "実用技能英語検定準1級に合格",
+    text: "公益財団法人日本英語検定協会が実施する日本の英語の語学検定に合格しました。",
+    timestamp: "2016年10月",
+    image: "eiken.png",
+  },
 ] satisfies Array<timeline>;
